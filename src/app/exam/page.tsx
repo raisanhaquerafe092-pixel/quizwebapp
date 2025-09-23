afc6e79953d2dbe36bb3719e135d75b2b35240dd
Bipul <surjosharma8888@gmail.com>
2025-09-23 18:37:07 +0600

    remove

--- src/app/exam/page.tsx
+++ src/app/exam/page.tsx
@@ -1,9 +1,14 @@
-
+import Image from "next/image"
+import Arr from  "@/../public/left-arrow.png"
+import Link from "next/link"
 export default function page() {
   return (
     <div>
-        <h1>This is a exam site</h1>
-      
+        <h1 className="text-4xl text-center">This is a exam site</h1>
+        <Link className="mt-10" href="/start">
+        <Image src={Arr} alt="left-arrow" width={35} height={35}/>
+        </Link>
+
     </div>
   )
 }
--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link"
 export default function page() {
   return (
    <div className="bg-[rgb(18,18,22)] text-[rgb(255,255,255)] h-screen">
-    <h1 className="text-5xl text-center" >Simple Study & Quiz Webapp</h1>
-    <div className="btns justify-center align-center grid gap-10 mt-10">
+    <h1 className="text-5xl text-center mt-5" >Simple Study & Quiz Webapp</h1>
+    <div className="btns justify-center align-center grid gap-10 mt-60">
     <button><Link href ="/exam">Exam</Link></button>
     <button><Link href ="/study">Study</Link></button>
     </div>
