edd8cdabb7d5e7d218faf0c20a72f7c8cacebd0e
Bipul <surjosharma8888@gmail.com>
2025-09-23 17:11:43 +0600

    all done

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -2,7 +2,7 @@
 export default function page() {
   return (
     <div>
-    <h1 className="text-base text-center text-blue-900 mt-[3%]" >Hellow Welcome to bipul Quiz web app </h1>
+    <h1 className="text-4xl text-center text-blue-900 mt-[3%]" >Hellow Welcome to bipul Quiz web app </h1>
     </div>
   )
 }
